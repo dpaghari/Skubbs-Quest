26e3e8cdf797ccbe7505f4bdb09322503f29f48f
John Bentley <jwbentle@ucsc.edu>
2013-08-13 23:48:02 -0700

    Added new board texture - Added wooden board texture

--- js/Game.js
+++ js/Game.js
@@ -137,9 +137,9 @@ Game.prototype.init = function() {
     var ambient_light = new THREE.AmbientLight(0x202020);
     this.scene.add(ambient_light);
     // Background plane
-    var bgTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+    var bgTexture = new THREE.ImageUtils.loadTexture( 'images/floor.jpg' );
     var bgMaterial = new THREE.MeshBasicMaterial( { map: bgTexture, side: THREE.DoubleSide } );
-    var bgplane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 900), bgMaterial);
+    var bgplane = new THREE.Mesh(new THREE.CubeGeometry(1000, 1000, 100), bgMaterial);
     bgplane.translateZ(-100);
     this.scene.add(bgplane);
     
@@ -304,6 +304,7 @@ Game.prototype.legalPosition = function(position) {
 
 // Function for creating a gem
 Game.prototype.createGem = function(position) {
+    this.randNum = Math.floor(Math.random() * (101));
     // If the random number is between 0 and 20 create a Diamond
     if(this.randNum >= 0.0 && this.randNum <= 20.0){
         this.nextGem = new diamondGem(this.nextPosition, this.scene);
@@ -338,7 +339,6 @@ Game.prototype.createGem = function(position) {
 		if (this.virtualBoard[-position.y + this.offset][position.x + this.offset].isEmpty) {
 	
 			// Calculate a random number between 0-100 to determine what gem you are shooting next
-			this.randNum = Math.floor(Math.random() * (101));
 			var newPosition = {
 				x : position.x,
 				y : position.y
